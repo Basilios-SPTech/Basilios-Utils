f4a8a8256fa2a90e7ef8da3816da32cdb31a08da
pedromorais03 <pedromorais3012@gmail.com>
2025-08-27 18:49:25 -0300

    created base skeleton for sql tables

---
+++ sql/produto.sql
@@ -0,0 +1,10 @@
+CREATE TABLE produto (
+    id_produto INT AUTO_INCREMENT PRIMARY KEY,
+    nome_produto VARCHAR(255) NOT NULL,
+    descricao TEXT,
+    ingredientes JSON,
+    preco DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    is_paused BOOLEAN DEFAULT FALSE
+);
